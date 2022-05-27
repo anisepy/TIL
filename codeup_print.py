df245831e0e6a7341f98d1c57387e86b336f6ff1
anisepy <heesu940@gmail.com>
2022-05-27 11:35:04 +0900

    feat : codeup site's print questions

---
+++ codeup_print.py
@@ -0,0 +1,14 @@
+# codeup print question 6005 ~ 6008  
+
+# 6005
+print('"hello world"')
+
+# 6006 : print "!@#$%^&*()'
+print('"!@#$%^&*()\'')
+
+# 6007
+print("C:\\Download\\'hello'.py")
+
+# 6008
+print('hello\nworld')
+print('hello\tworl')
