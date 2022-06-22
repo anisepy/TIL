75bb5ad4f49d6f74c8a2c2e7b3863660f50a00fe
anisepy <heesu940@gmail.com>
2022-06-22 20:51:45 +0900

    fix : fix data's route

--- ml-project/utils.py
+++ ml-project/utils.py
@@ -2,7 +2,7 @@ import json
 
 
 def load_config(model_name):
-    with open(f'ml-project/config/{model_name}.json', 'r', encoding='utf-8') as f:
+    with open(f'./config/{model_name}.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
 
     return data
