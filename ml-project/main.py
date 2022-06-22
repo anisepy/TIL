2e4072075f9ded97fbbabe420390f2f90837d073
anisepy <heesu940@gmail.com>
2022-06-22 20:39:08 +0900

    fix : update for catboost modeling

--- ml-project/main.py
+++ ml-project/main.py
@@ -4,73 +4,88 @@ from utils import load_config
 from preprocess import scaling
 from preprocess import reduce_mem_usage
 from preprocess import preprocess_text
+from preprocess import preprocess_text_test
+from visualization import visualize_feature_importance
+from visualization import get_top_bottom_coef
+from visualization import visualize_coefficient
 
-
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LinearRegression  # 1. Linear Regression
-from sklearn.linear_model import Lasso             # 2. Lasso
-from sklearn.linear_model import Ridge             # 3. Ridge
-from xgboost.sklearn import XGBRegressor           # 4. XGBoost
-from lightgbm.sklearn import LGBMRegressor         # 5. LightGBM
+import catboost   
+from xgboost.sklearn import XGBRegressor           
+from lightgbm.sklearn import LGBMRegressor   
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
-from sklearn import utils
+import warnings
+warnings.filterwarnings('ignore')
 
 def main():
     # TODO python argparse package(library)
     # python main.py --config rf.json
-    # python main.py --config svm.json
-    # python main.py --config lr.json
+    # python main.py --config cb.json
+    # python main.py --config xgb.json
     # python main.py --config lgbm.json
-
+    
+    # load data
     df, target = load_train()
-    # test = load_test()
-
-    cfg = load_config(model_name='lgbm')
+    test, submission = load_test()
+    df = df.sample(n=500000)
+    print("success data loading")
+    
+    # json file option
+    cfg = load_config(model_name='cb')
     preprocess_cfg = cfg['preprocess']
     model_cfg = cfg['params']
 
+    # preprocessing
     if preprocess_cfg['text']:
-        preprocess_text(df)
-        # preprocess_text(test)
+        df = preprocess_text(df)
+        test = preprocess_text_test(test)
 
     reduce_mem_usage(df)
-    # reduce_mem_usage(test)
+    reduce_mem_usage(test)
 
+    # data split
     X_train, X_val, y_train, y_val = train_test_split(df.drop(target, axis=1), df[target],
                                                     test_size=0.1, random_state=42)
-    # X_test = test
-                                                   
+    X_test = test
+
     print(X_train.shape, X_val.shape, y_train.shape, y_val.shape)
 
-
+    # feature scaling
     if preprocess_cfg['scaling']:
-        X_train = scaling(X_train)
+        X_train_sc = scaling(X_train)
         X_val = scaling(X_val)
-        # X_test = scaling(X_test)
-        # lab_enc = preprocessing.LabelEncoder()
-        # X_train = lab_enc.fit_transform(X_train)
-        # X_val = lab_enc.fit_transform(X_val)
+        X_test = scaling(X_test)
 
-    model = LGBMRegressor()
+    # training 
+    # model = LGBMRegressor()
     # model = LGBMRegressor(**model_cfg)
-    model.fit(X_train, y_train)
-    pred_train = model.predict(X_train)
+    # model = XGBRegressor()
+    # model = XGBRegressor(**model_cfg)
+    model = catboost.CatBoostRegressor()
+    # model = catboost.CatBoostRegressor(**model_cfg)
+    model.fit(X_train_sc, y_train)
+    pred_train = model.predict(X_train_sc)
     pred_val = model.predict(X_val)
 
     mae_train = mean_absolute_error(y_train, pred_train)
     mae_val = mean_absolute_error(y_val, pred_val)
 
-    print("LightGBM MAE result,\t train = %.4f, val = %.4f" % (mae_train, mae_val))
-    # print("Random Forest MAE result,\t train = %.4f, val = %.4f" % (mae_train, mae_val))
-    
-    # result = model.predict(X_test)
-    # print("---------- RandomForest ---------")
-    # print("---------- LightGBM ---------")
-    # print('MSE in training: %.4f' % mean_absolute_error(y_test, result))
-    # print(" ")
-    # print("Done")
+    # print("Light GBM MAE result,\t train = %.5f, val = %.5f" % (mae_train, mae_val))
+    # print("XGBoost MAE result,\t train = %.5f, val = %.5f" % (mae_train, mae_val))
+    print("CatBoost MAE result,\t train = %.5f, val = %.5f" % (mae_train, mae_val))
+
+
+    # test prediction & maiking submission file
+    result = model.predict(X_test)
+    submission[target] = result
+    submission.to_csv("./data/submission.csv", index = False)
+
+    # visualization
+    if preprocess_cfg['importance']:
+        visualize_feature_importance(model,X_train)
+    if preprocess_cfg['coefficient']:
+        get_top_bottom_coef(model, X_train)
+        visualize_coefficient(model)
     # TODO evaluate
 
 
